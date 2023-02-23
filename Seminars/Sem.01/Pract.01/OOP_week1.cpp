fddeb5f378cbfabea9a933678da0b2a75f78b727
ghald03 <georgi.hal@abv.bg>
2023-02-23 19:33:49 +0200

    Create OOP_week1.cpp

---
+++ Seminars/Sem.01/Pract.01/OOP_week1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int biggestNumberByValue(int a, int b) {
+    return (a > b ? a : b);
+}
+
+int biggestNumberByReference(const int& a, const int& b) {
+    return biggestNumberByValue(a, b);
+}
+
+int biggestNumberByPointer(int* const a, int* const b) {
+    return biggestNumberByValue(*a, *b);
+}
+
+
+int main() {
+    
+    int a, b;
+    
+    std::cout << "First number: ";
+    std::cin >> a;
+    std::cout << "Second number: ";
+    std::cin >> b;
+
+    int* c = new int(a);
+    int* d = new int(b);
+    
+    std::cout <<
+    biggestNumberByPointer(c, d) << " " <<
+    biggestNumberByReference(a, b) << " " <<
+    biggestNumberByValue(a, b);
+
+    delete c;
+    delete d;
+
+    return 0;
+}
